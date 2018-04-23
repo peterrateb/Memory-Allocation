2a75fbaf8b441ebd1f017ed8394ae5a190f87f7e
Peter Rateb <31657506+peterrateb@users.noreply.github.com>
2018-04-23 20:37:53 +0200

    Update datatypes.cs

--- datatypes.cs
+++ datatypes.cs
@@ -1,13 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace dataTypes
 {
-	struct block {
-		public string name;
-		public float startaddress;
-		public float size;
-	}
+    struct block
+    {
+        public string name;
+        public int startaddress;
+        public int size;
+    }
 }
